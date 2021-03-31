c42bdef955aa17ee0bd167c4eb9b45445515c83f
Marie-Louise <mariemuteteyimana@gmail.com>
2021-04-01 01:04:10 +0300

    assgnment.jjs

---
+++ marie.js
@@ -0,0 +1,15 @@
+const name='marie';
+const age=20;
+console . log('my name is' + name + ' and I am ' + age);
+
+
+
+function food(){
+    var food=["rice","meat","cheeps","beans",];
+    food.forEach(food=>{
+        console. log(food);
+
+        });
+
+        }
+       food()
